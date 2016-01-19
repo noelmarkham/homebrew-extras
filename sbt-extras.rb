5df2640ecc525644ade04f7cd9ad677669e3cf72
Noel Markham <noel.markham@gmail.com>
2016-01-19 09:51:22 +0100

    update sha for new sbt-extras

--- sbt-extras.rb
+++ sbt-extras.rb
@@ -3,7 +3,7 @@ class SbtExtras < Formula
   version "0.13.9"
   homepage "https://github.com/paulp/sbt-extras"
   url "https://github.com/paulp/sbt-extras/archive/master.zip"
-  sha256 "6dd760c3da7d418425ca358265c5226f3868131156a7e48e03cbcdbae0b8ef75"
+  sha256 "6de80c9252a82d59c41dbe918a3bb91562e0d8527709282fcedd3997fb6051ec"
 
   def install
     bin.install "sbt"
