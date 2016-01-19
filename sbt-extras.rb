d83e941329cca0419463f2ec86fe3e987dfa615b
Noel Markham <noel.markham@gmail.com>
2016-01-19 09:56:16 +0100

    Version update

--- sbt-extras.rb
+++ sbt-extras.rb
@@ -1,6 +1,6 @@
 class SbtExtras < Formula
   desc "SBT Extras by Paul Phillips"
-  version "0.13.9"
+  version "0.13.9-6de80c"
   homepage "https://github.com/paulp/sbt-extras"
   url "https://github.com/paulp/sbt-extras/archive/master.zip"
   sha256 "6de80c9252a82d59c41dbe918a3bb91562e0d8527709282fcedd3997fb6051ec"
