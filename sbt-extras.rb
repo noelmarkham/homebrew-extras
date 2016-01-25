36f646c554559b8075eff0fbf2d4a388ae83af2e
Noel Markham <noel.markham@gmail.com>
2016-01-25 14:18:40 +0000

    Using the proper head function

--- sbt-extras.rb
+++ sbt-extras.rb
@@ -1,10 +1,6 @@
-#curl -L https://github.com/paulp/sbt-extras/archive/master.zip | shasum -a 256
 class SbtExtras < Formula
   desc "SBT Extras by Paul Phillips"
-  version "0.13.9-6835a"
-  homepage "https://github.com/paulp/sbt-extras"
-  url "https://github.com/paulp/sbt-extras/archive/master.zip"
-  sha256 "6835a27010687a04ae87485f3254ef9a3054dd685309842309930bd43d507090"
+  head "https://github.com/paulp/sbt-extras.git"
 
   def install
     bin.install "sbt"
