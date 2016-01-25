55609a3066f140b7b286c83a79f7112c67ba6c22
Noel Markham <noel.markham@gmail.com>
2016-01-25 12:25:48 +0000

    better sha256

--- sbt-extras.rb
+++ sbt-extras.rb
@@ -1,10 +1,10 @@
 #curl -L https://github.com/paulp/sbt-extras/archive/master.zip | shasum -a 256
 class SbtExtras < Formula
   desc "SBT Extras by Paul Phillips"
-  version "0.13.9-93a46f"
+  version "0.13.9-6835a"
   homepage "https://github.com/paulp/sbt-extras"
   url "https://github.com/paulp/sbt-extras/archive/master.zip"
-  sha256 "93a46fb937358056761923e7e44073921207df1463291a39e34de58c3c81c80a"
+  sha256 "6835a27010687a04ae87485f3254ef9a3054dd685309842309930bd43d507090"
 
   def install
     bin.install "sbt"
